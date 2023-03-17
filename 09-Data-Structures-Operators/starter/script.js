075db86c4eb8ef23123918760045fb39a1b91feb
Nick Williams <nickfwilliams@gmail.com>
2023-03-17 16:45:07 +0000

    Finish up chapter on Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -3,6 +3,7 @@
 //MAPS
 
 const rest = new Map();
+
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
 console.log(rest.set(2, 'Lisbon, Portugal'));
@@ -31,7 +32,17 @@ console.log(rest.size);
 
 console.log(rest.get(arr));
 
-/*
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try Again'],
+]);
+
+console.log(question);
 
 // SETS
 
@@ -67,9 +78,6 @@ console.log(
   new Set(['Waiter', 'Chef', 'Waiter', 'Manger', 'Chef', 'Waiter']).size
 );
 
-
-
-
 const weekdays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
 
 const openingHours = {
@@ -86,6 +94,32 @@ const openingHours = {
     close: 24,
   },
 };
+
+//Convert Object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz App
+
+console.log(question.get('question'));
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert Map to Array
+
+console.log([...question]);
+
+/*
+
 
 // Data needed for first part of the section
 const restaurant = {
