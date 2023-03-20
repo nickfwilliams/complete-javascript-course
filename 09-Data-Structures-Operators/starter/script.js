9840906d5ae7ebc30be99458cfda29a88fb7391b
Nick Williams <nickfwilliams@gmail.com>
2023-03-20 17:29:22 +0000

    Update Working with Strings - Part 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,38 @@
 'use strict';
 
+//STRINGS
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are the middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat :-(');
+  else console.log('You got lucky! :-D');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+/*
+
 //MAPS
 
 const rest = new Map();
@@ -31,7 +64,7 @@ console.log(rest.size);
 
 console.log(rest.get(arr));
 
-/*
+
 
 // SETS
 
