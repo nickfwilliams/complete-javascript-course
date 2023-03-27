af0de6f4b3019a14beb21db28730220b541ce07c
Nick Williams <nickfwilliams@gmail.com>
2023-03-27 17:15:26 +0100

    A Closer Look at Functions - Lesson 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const bookings = [];
+
+// 1: A Closer Look at Functions
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // Old ES5 way
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  // ES6 Way
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+//Skipping an default parameter, use undefined
+createBooking('LH123', undefined, 1000);
