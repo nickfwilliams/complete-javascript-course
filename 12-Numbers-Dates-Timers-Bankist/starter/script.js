34b54a7a2ff6e136391899f330abf5a9d6c37399
Nick Williams <nick.williams@bristol.ac.uk>
2023-08-11 15:44:29 +0100

    Complete timeout and interval chapter

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -444,3 +444,23 @@ future.setFullYear(2040);
 console.log(future);
 
 */
+
+// 7. Timers
+
+// setTimeout
+
+const ingredients = ['olives', 'mushrooms'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza! with ${ing1} and ${ing2}`),
+  3000,
+  ...ingredients
+); // Delayed by three seconds
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(() => {
+  const now = new Date();
+  console.log(now);
+}, 1000);
