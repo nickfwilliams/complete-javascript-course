f56a2ba302eb1c85e3a7f5d5d61f8556fae0f7aa
Nick Williams <nick.williams@bristol.ac.uk>
2023-08-18 16:41:02 +0100

    Update Advanced DOM chapter

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,6 +30,11 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+/*
+
+
+// Lesson 1 - Selecting, creating and deleting
+
 // Selecting elements
 
 console.log(document.documentElement);
@@ -68,3 +73,140 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+// Lesson 2 - Styles, Attributes and Classes
+
+// Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color); // Doesn't work because we haven't set the inline style using JS
+console.log(message.style.backgroundColor); // DOES work because we HAVE set the inline style using JS
+
+console.log(getComputedStyle(message).color); // This gets the style even if we haven't set it using JS
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard way to get attributes
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('Company', 'Bankist');
+
+console.log(logo.src); // Absolute URL
+console.log(logo.getAttribute('src')); // Relative URL
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href')); // Relative URL
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+//Don't use this as it overrides all classes and only allows one class
+logo.className = 'jonas';
+
+
+
+// Lesson 3. Implementing Smooth Scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // Old view to smooth scroll
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // New way to smooth scroll
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+// Lesson 4. Types of Events and Event Handlers
+
+const h1 = document.querySelector('h1');
+
+// More modern way - this allows you to add multiple event listeners to the same event
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// Old school way - can only attach one listener to one event
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+*/
+
+// Lesson 5. Bubbling and Capturing
+
+// Lesson 6. Event Propagation in Practice
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
