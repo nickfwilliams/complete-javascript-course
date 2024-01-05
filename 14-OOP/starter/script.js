853096c3117f072abfd5d5a7bb3bb82139752ce1
Nick Williams <nickfwilliams@gmail.com>
2024-01-05 15:32:56 +0000

    Complete Lesson 208 - Constructor Functions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never create a method inside of a constructor function because you copy this with every new instance
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const nick = new Person('Nick', 1983);
+console.log(nick);
+
+// 1. A new {} is created
+// 2. Function is called, this = {}
+// 3. {} linked to a prototype
+// 4. Function automatically returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(nick instanceof Person);
